f89fd801fdda2c8af44a52595f09ebe2e681ca83
Cristobal <cristobal.isla@ug.uchile.cl>
2025-12-06 15:42:44 -0300

    day2 2

--- day2.py
+++ day2.py
@@ -1,11 +1,50 @@
 import sys
 
-input = open(sys.argv[1]).read().rstrip()
-ranges = input.split(",")
+input_ = open(sys.argv[1]).read().rstrip()
+ranges = input_.split(",")
+
+
+def getDivisors(n):
+    divisors = []
+    if n == 1:
+        return [1]
+    for i in range(1, n):
+        if n % i == 0:
+            divisors.append(i)
+    return divisors
+
+def getInvalids(l, r):
+    digitsl = len(str(l))
+    digitsr = len(str(r))
+    invalids = []
+    if digitsl < digitsr:
+        invalids += getInvalids(10**digitsl, r)
+        r = (10**digitsl) - 1
+    divisors = getDivisors(digitsl)
+    for n in divisors:
+        repetitions = digitsl // n
+        values = []
+        min = int(str(l)[:n])
+        max = int(str(r)[:n])
+        possible_patterns = [min] if min == max else range(min, max + 1)
+        for pattern in possible_patterns:
+            number = 0
+            for i in range(repetitions):
+                number += pattern * (10 ** (i * n))
+            if l <= number and number <= r:
+                values.append(number)
+
+        invalids += values
+    return invalids
+
 
 result = 0
+result2 = 0
+invalids = []
 for interval in ranges:
     l, r = interval.split("-")
+    values = getInvalids(int(l), int(r))
+    invalids += values
     for i in range(int(l), int(r) + 1):
         number = str(i)
         length = len(number)
@@ -15,3 +54,4 @@ for interval in ranges:
                 result += i
 
 print(result)
+print(sum({x for x in set(invalids) if x > 9}))
